6e4a18e59d42a8758d0986369674e778516c548d
Macbook Pro <macbookpro@MacBook-Pro-cua-Macbook.local>
2024-06-20 09:38:58 +0700

    handle data

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hive/hive.dart';
 import 'package:login_screen/models/fake_account.dart';
 import 'package:login_screen/screens/custom/custom_dialog.dart';
 import 'package:login_screen/screens/custom/custom_iconbutton.dart';
@@ -22,15 +23,30 @@ class _LoginPageState extends State<LoginPage> {
   bool _showCloseIcon = false;
   bool _showEyeIcon = false;
   AutovalidateMode _validateMode = AutovalidateMode.disabled;
+  var loginBox = Hive.box('userBox');
 
   @override
   void initState() {
     super.initState();
+    _loadData();
     _taxCodeController.addListener(_updateCloseIcon);
     _passwordController.addListener(_updateEyeIcon);
   }
 
+  void _putData() async {
+    var loginBox = Hive.box('userBox');
+    await loginBox.put('isLoggedIn', true);
+    await loginBox.put('taxCode', _taxCodeController.text);
+    await loginBox.put('account', _accountController.text);
+    await loginBox.put('password', _passwordController.text);
+  }
 
+  void _loadData() {
+    var loginBox = Hive.box('userBox');
+    _taxCodeController.text = loginBox.get('taxCode',defaultValue: '');
+    _accountController.text = loginBox.get('account',defaultValue: '');
+    _passwordController.text = loginBox.get('password',defaultValue: '');
+  }
 
   void _updateCloseIcon() {
     setState(() {
@@ -61,7 +77,7 @@ class _LoginPageState extends State<LoginPage> {
       if (_taxCodeController.text == FakeAccount.fakeAccount.taxCodeFake &&
           _accountController.text == FakeAccount.fakeAccount.accountFake &&
           _passwordController.text == FakeAccount.fakeAccount.passwordFake) {
-
+        _putData();
         Navigator.pushReplacement(
             context,
             MaterialPageRoute(
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:login_screen/screens/login_page.dart';
+import 'package:login_screen/screens/home_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () async {
+      var loginBox = Hive.box('userBox');
+      bool isLoggedIn = loginBox.get('isLoggedIn', defaultValue: false);
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => isLoggedIn ? const HomePage() : const LoginPage(),
+        ),
+      );
+    });
+
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
